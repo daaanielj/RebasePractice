45531f4177eab748e1b7b10f587608a13dd7927f
Daniel Jiang <daaanielj@outlook.com>
2025-09-06 11:00:30 -0700

    add conflicts on main

--- utils.ts
+++ utils.ts
@@ -28,6 +28,23 @@ export class TodoManager {
 
     listTodos(): Todo[] {
         return this.todos;
+  }
+      addTodo(title: string) {
+        const id = this.todos.length + 1;
+        this.todos.push({ id, title, completed: false });
+    }
+
+    removeTodo(id: number) {
+        this.todos = this.todos.filter(todo => todo.id !== id);
+    }
+
+    toggleTodo(id: number) {
+        const todo = this.todos.find(todo => todo.id === id);
+        if (todo) todo.completed = !todo.completed;
+    }
+
+    listTodos(): Todo[] {
+        return this.todos;
     }
 }
 
